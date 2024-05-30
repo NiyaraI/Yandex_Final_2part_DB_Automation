6bf79af33c01bc074d613049c56095718cdf0f56
NiyaraI <niyara984@gmail.com>
2024-05-30 16:15:10 +0400

    ok

---
+++ config.py
@@ -0,0 +1,5 @@
+
+
+stand_url = "https://c984e40b-202c-45c9-9b8f-06e112dd8137.serverhub.praktikum-services.ru"
+post_order = "/api/v1/orders"
+get_order_id_from_track = "/api/v1/orders/track"
---
+++ sender_request_test.py
@@ -0,0 +1,24 @@
+# Выполнить запрос на создание заказа.
+# Сохранить номер трека заказа.
+# Выполнить запрос на получения заказа по треку заказа.
+# Проверить, что код ответа равен 200.
+
+import requests
+import config
+import data
+
+def get_order_track():
+    responce = requests.post(config.stand_url + config.post_order, json=data.order_body)
+    return responce.json()['track']
+
+track = get_order_track()
+print("track is", track)
+
+def get_order_id(id):
+    responce = requests.get(config.stand_url + config.get_order_id_from_track+ "?t=" + str(id))
+    assert responce.status_code == 200
+    return responce.json()['order']['id']
+
+get_id = get_order_id(track)
+print("id is", get_id)
+
